b648fbe4c0b70209adc0625ff57d6669380efe33
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-19 12:32:22 +0530

    Making JS read the quote on clicking on sound button
    1. The  SpeechSynthesisUtterance() is a web speech API that represents a speech request
    
    2. speechSynthesis.speak(utterance) ---  this method speaks the utterance
    
    3. Its damnnnn cool

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const quote = document.querySelector(".quote");
 const author = document.querySelector(".author .name");
 const quoteBtn = document.querySelector("button");
+const soundBtn = document.querySelector(".sound");
+const copyBtn = document.querySelector(".copy");
+const twitterBtn = document.querySelector(".twitter");
 
 const randomQuote = () => {
   quoteBtn.classList.add("loading");
@@ -18,3 +21,10 @@ const randomQuote = () => {
 };
 
 quoteBtn.addEventListener("click", randomQuote);
+
+soundBtn.addEventListener("click", () => {
+  let utterance = new SpeechSynthesisUtterance(
+    `${quote.textContent} by ${author.textContent}`
+  );
+  speechSynthesis.speak(utterance);
+});
