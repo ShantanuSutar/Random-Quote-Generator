db77022e05e79daa072c5395c5fcfc578d5c748f
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-19 12:23:12 +0530

    Styling button on loading

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ const author = document.querySelector(".author .name");
 const quoteBtn = document.querySelector("button");
 
 const randomQuote = () => {
+  quoteBtn.classList.add("loading");
   fetch("https://api.quotable.io/random")
     .then((res) => res.json())
     .then((result) => {
       console.log(result);
       quote.textContent = result.content;
       author.textContent = result.author;
+      quoteBtn.classList.remove("loading");
+      quoteBtn.textContent = "New Quote";
     });
 };
 
